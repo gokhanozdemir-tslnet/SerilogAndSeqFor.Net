40571869fe1df7feec04c851c32754b18cb352e8
gokhanozdemir-tslnet <102428631+gokhanozdemir-tslnet@users.noreply.github.com>
2023-07-04 15:49:43 +0300

    deleted folder

--- Serilog/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Serilog.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
