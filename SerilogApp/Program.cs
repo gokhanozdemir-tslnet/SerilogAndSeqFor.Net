ef20441b4f6a76fb31f2482cae879cc5f6fd1416
gokhanozdemir-tslnet <102428631+gokhanozdemir-tslnet@users.noreply.github.com>
2023-07-04 15:42:04 +0300

    Add project files.

---
+++ SerilogApp/Program.cs
@@ -0,0 +1,37 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+//Serilog
+//The builder.Host.UseSerilog() call will redirect all log events through your Serilog pipeline.
+builder.Host.UseSerilog(
+    (HostBuilderContext context, IServiceProvider services, LoggerConfiguration loggerConfiguration)
+    =>
+    {
+        loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services);
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
